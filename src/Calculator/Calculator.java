c0ddfb1aa3276106042264a133b0f30c183b12d8
hi <therod@nate.com>
2014-11-28 18:10:26 +0900

    텍스트 필드 추가

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,13 +1,25 @@
 package Calculator;
+import java.awt.Frame;
+import java.awt.TextField;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class Calculator {
+class Calculator extends Frame{
+    public Calculator(){
+        super("계산기");
+        init();
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){//e 가 아니라 다른거 써도 됨
+				System.exit(0);
+			}
+		});//아답터를 사용하면 전부다 정의 필요X
+        this.setSize(300, 300);
+        this.setLocation(500, 200);
+        this.setVisible(true);
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private void init() {
+		TextField tf = new TextField("0");
+		this.add("North",tf);
 	}
-
 }
