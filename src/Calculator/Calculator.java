445cd6d9affd15e0ad400b3ddaeedddc90e9dd1e
hi <therod@nate.com>
2014-11-28 19:23:21 +0900

    label > textfield 종료이벤트 배운걸로

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,31 +1,30 @@
 package Calculator;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
 public class Calculator extends JFrame {
-	JLabel label;
+//	JLabel label;
+	TextField tf;
 	JButton bNum[] = new JButton[10]; 
 	JButton plus, minus, multi, div, equal, clear; 
 	String inputValue; 
 	int result; 
 	char lastOp;
 
-	public static void main(String[] args) {
-		new Calculator();
-	}
-
 	public Calculator() {
 		super("계산기");
-		setBounds(200, 200, 300, 300);
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300,400);
+		setVisible(true);
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
 
-		label = new JLabel("0", JLabel.RIGHT);
-
-		add(label, BorderLayout.NORTH);
+		tf = new TextField("0");
+		add(tf, BorderLayout.NORTH);
 
 		JPanel p = new JPanel(new GridLayout(4, 4, 2, 2));
 		add(p, BorderLayout.CENTER);
@@ -41,23 +40,10 @@ public class Calculator extends JFrame {
 		for (i = 0; i < bNum.length; i++) {
 			bNum[i] = new JButton(Integer.toString(i));
 		}
-
-		p.add(bNum[7]);
-		p.add(bNum[8]);
-		p.add(bNum[9]);
-		p.add(plus);
-		p.add(bNum[4]);
-		p.add(bNum[5]);
-		p.add(bNum[6]);
-		p.add(minus);
-		p.add(bNum[1]);
-		p.add(bNum[2]);
-		p.add(bNum[3]);
-		p.add(multi);
-		p.add(bNum[0]);
-		p.add(equal);
-		p.add(clear);
-		p.add(div);
+		p.add(bNum[7]);p.add(bNum[8]);p.add(bNum[9]);p.add(plus);
+		p.add(bNum[4]);p.add(bNum[5]);p.add(bNum[6]);p.add(minus);
+		p.add(bNum[1]);p.add(bNum[2]);p.add(bNum[3]);p.add(multi);
+		p.add(bNum[0]);p.add(equal);  p.add(clear);  p.add(div);
 
 		NumberHandler nh = new NumberHandler();
 		for (i = 0; i < bNum.length; i++) {
@@ -77,12 +63,16 @@ public class Calculator extends JFrame {
 	class NumberHandler implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			String s = e.getActionCommand(); // 클릭한 버튼의 레이블
-			inputValue = new String(); // 첫 자리이면 String 객체 생성
-			
+			if (inputValue == null) {
+				if (s.equals("0"))
+					return; 
+				else
+					inputValue = new String(); // 첫 자리이면 String 객체 생성
+			}
 			if (inputValue.length() >= 9)
 				return; // 9자리 넘으면 무시
 			inputValue += s; // 두번째 이후이면 누른 숫자 덧붙이기
-			label.setText(inputValue); // 현재 숫자 화면에 표시
+			tf.setText(inputValue); // 현재 숫자 화면에 표시
 		}
 	}
 
@@ -90,15 +80,13 @@ public class Calculator extends JFrame {
 		public void actionPerformed(ActionEvent e) {
 			JButton source = (JButton) e.getSource();
 			int value;
-
 			if (source == clear) {
-				label.setText("0"); 
+				tf.setText("0"); 
 				inputValue = null; 
 				lastOp = 0; 
 				result = 0; 
 				return;
 			}
-			// 기타 계산 +,-,*,/
 			if (inputValue != null) {
 				value = Integer.parseInt(inputValue);
 				switch (lastOp) {
@@ -118,7 +106,7 @@ public class Calculator extends JFrame {
 					result = value;
 					break;
 				}
-				label.setText(Integer.toString(result));
+				tf.setText(Integer.toString(result));
 			}
 			inputValue = null;
 			lastOp = source.getText().charAt(0);
